aeb8b4e2dcb3f9b90aa60a4a45f8e3879d9d3e96
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-10-31 23:47:41 +0100

    add tests

---
+++ mod_test.ts
@@ -0,0 +1,43 @@
+import { assertEquals } from "@std/assert";
+import { Cache } from "./mod.ts";
+
+Deno.test(function testSet() {
+    const cache = new Cache<string, number>();
+
+    assertEquals(cache.set("a", 0), true);
+    assertEquals(cache.get("a"), 0);
+
+    assertEquals(cache.get("b"), undefined);
+    assertEquals(cache.set("b", 0), true);
+    assertEquals(cache.get("b"), 0);
+
+    assertEquals(cache.set("b", 1), false);
+    assertEquals(cache.get("b"), 1);
+    assertEquals(cache.get("a"), 0);
+
+    assertEquals(cache.get("c"), undefined);
+});
+
+Deno.test(async function testFetch() {
+    const cache = new Cache<number, string>({
+        ttl: 1_000, // one second
+    });
+
+    cache.set(0, "foo");
+    assertEquals(cache.get(0), "foo");
+    await new Promise(resolve => setTimeout(resolve, 1_000))
+    assertEquals(cache.contains(0), false);
+    assertEquals(cache.get(0), undefined);
+});
+
+Deno.test(function testCompare() {
+    const cache = new Cache<string, string>({
+        compare: (a: string, b: string): boolean => {
+            return a.toLowerCase() === b.toLowerCase();
+        },
+    });
+
+    cache.set("Hello", "World")
+    assertEquals(cache.contains("hello"), true);
+});
+
